90c89a66a3290b42fe728e78c353c6d680899b9f
Kevin Lewin <lewinke@amazon.com>
2022-11-02 15:17:49 -0400

    cli output

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -23,6 +23,7 @@ func main() {
 		log.Printf("%v", err)
 	}
 
+	fmt.Printf("OTEL Config file %v is valid\n", *filename)
 	fmt.Println("Pipelines: ")
 	for i, v := range cfg.Pipelines {
 		fmt.Printf("%s: Receivers = %s , Processors = %s , Exporters = %s\n", i, v.Receivers, v.Processors, v.Exporters)
